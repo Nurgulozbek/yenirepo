2b4d4bd0ca382115b303829a124393e1a3059e03
NurgulOzbek <gsgulnur74@gmail.com>
2023-05-06 22:25:34 +0300

    v2

--- src/main/java/Class1.java
+++ src/main/java/Class1.java
@@ -8,19 +8,7 @@ public class Class1 {
         System.out.println(x+y);
         System.out.println("Version 1 icin islemler tamam");
 
-        //version 2 çalışmaları...
-        int z = 4;
-        System.out.println(x+y+z);
-
-        //v3
-        System.out.println("v3 kodları...");
-
-        //v4
-        System.out.println("v4 kodları...");
-
-        //v5 kodları
-
-        //v6 için yeni kodlar
+        //v2 olustur
 
     }
 }
