c59263ba5627f3a3050a2f67f252cf1b57601994
NurgulOzbek <gsgulnur74@gmail.com>
2023-05-06 20:02:26 +0300

    version 1

---
+++ src/main/java/Class1.java
@@ -0,0 +1,11 @@
+public class Class1 {
+
+    public static void main(String[] args) {
+
+
+        int x = 1;
+        int y = 2;
+        System.out.println(x+y);
+        System.out.println("Version 1 icin islemler tamam");
+    }
+}
